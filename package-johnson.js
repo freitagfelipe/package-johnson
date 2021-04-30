f6bda7528714ce7e60b2df5faa87857130ccb876
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-04-30 18:26:00 -0300

    Add message control in log

--- package-johnson.js
+++ package-johnson.js
@@ -6,7 +6,13 @@ dotenv.config();
 
 client.on("ready", () => {
     console.log("I'm Package Johnson and I'm ready!");
-    client.user.setActivity("Say hi to Package Johnson!");
+    client.user.setActivity("Say hi to @Package Johnson!");
+});
+
+client.on("message", message => {
+    if(message.author.id != "836491530394533908") {
+        console.log(`${message.author.username}: ${message.content}`);
+    }
 });
 
 client.login(process.env.TOKEN);
