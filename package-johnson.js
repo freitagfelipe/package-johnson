5b49bc130b20998724677ac7c18da46abcb20578
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-07 16:11:09 -0300

    Remove greeting message and add command message section

--- package-johnson.js
+++ package-johnson.js
@@ -1,25 +1,42 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const dotenv = require("dotenv");
+const fileSystem = require("fs");
+
+const client = new Discord.Client();
+const { prefix } = require("./config.json");
 
 dotenv.config();
 
 client.on("ready", () => {
     console.log("I'm Package Johnson and I'm ready!");
     client.user.setActivity("Say hi to @Package Johnson!");
-});
 
-client.on("message", message => {
-    if(message.author.id != "836491530394533908") {
-        console.log(`${message.author.username}: ${message.content}`);
+    client.commands = new Discord.Collection();
 
-        if(message.content.toLowerCase() == "hi <@!836491530394533908>" && message.mentions.users.first().id == "836491530394533908")
-            greetingMessage(message);
+    const commandFiles = fileSystem.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
     }
 });
 
-function greetingMessage(message) {
-    message.reply('hiii!!!! If you wanna see my command list just type: "!pj commands".');
-}
+client.on("message", message => {
+    if(message.author.id != client.user.id && message.content.startsWith(prefix)) {
+        console.log(`${message.author.username}: ${message.content}`);
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName);
+
+        try {
+            command.execute(message, args);
+        } catch (error) {
+            console.log(error);
+            message.reply("An error occurred while trying to execute your command");
+        }
+    }
+});
 
 client.login(process.env.TOKEN);
