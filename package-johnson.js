ded313cfad0d6046a01de4e3cfbef9cc23a622f1
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-07 16:33:27 -0300

    Add interactions section

--- package-johnson.js
+++ package-johnson.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 const fileSystem = require("fs");
+const { measureMemory } = require("vm");
 
 const client = new Discord.Client();
 const { prefix } = require("./config.json");
@@ -12,12 +13,19 @@ client.on("ready", () => {
     client.user.setActivity("Say hi to @Package Johnson!");
 
     client.commands = new Discord.Collection();
+    client.interations = new Discord.Collection();
 
     const commandFiles = fileSystem.readdirSync("./commands").filter(file => file.endsWith(".js"));
+    const interationFiles = fileSystem.readdirSync("./interations").filter(file => file.endsWith(".js"));
 
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
         client.commands.set(command.name, command);
+    }
+
+    for (const file of interationFiles) {
+        const interation = require(`./interations/${file}`);
+        client.interations.set(interation.name, interation);
     }
 });
 
@@ -35,6 +43,23 @@ client.on("message", message => {
         } catch (error) {
             console.log(error);
             message.reply("An error occurred while trying to execute your command");
+        }
+
+    } else if(message.author.id != client.user.id && message.mentions.users.array().length > 0 && message.mentions.users.first().id == client.user.id) {
+        console.log(`${message.author.username}: ${message.content}`);
+
+        let interationName;
+
+        if(message.content.toLowerCase().startsWith("hi")) {
+            interationName = "greeting message";
+        }
+
+        const interation = client.interations.get(interationName);
+
+        try {
+            interation.execute(message);
+        } catch (error) {
+            console.log(error);
         }
     }
 });
