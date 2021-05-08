e9a8d2061d391c82d55fc9febbf01f8d4c382453
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-08 11:46:31 -0300

    Add link.js command

---
+++ commands/link.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "link",
+    description: "Shows the link to add Package Johnson to your discord",
+
+    execute(message) {
+        const embedLink = new MessageEmbed()
+            .setAuthor(
+                "Package Johnson",
+                "https://i.imgur.com/vDVSu00.png"
+            )
+            .setColor("#FFFF00")
+            .setTitle("To add me in your discord server you just need to click on this message!")
+            .setURL(`https://bit.ly/package-johnson-bot`)
+            .addFields(
+                { name: "Created by:", value: "[Felipe Freitag](https://github.com/freitagfelipe)", inline: true},
+                { name: "Link to repository:", value: "[Github](https://github.com/freitagfelipe/package-johnson)", inline: true}
+            )
+
+        message.channel.send(embedLink);
+    }
+}
