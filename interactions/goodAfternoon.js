04ec4128a0020360119491d522c15697d0330901
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-14 17:02:05 -0300

    Update goodMorning, goodAfternoon and goodNight return message

--- interactions/goodAfternoon.js
+++ interactions/goodAfternoon.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "good afternoon",
 
     execute(message) {
-        const goodNightEmbed = new MessageEmbed()
+        return message.channel.send(new MessageEmbed()
             .setAuthor(
                 `${message.client.user.username}`,
                 `${message.client.user.displayAvatarURL()}`
@@ -12,7 +12,6 @@ module.exports = {
             .setColor("#FFFF00")
             .setTitle(`Kon'nichiwaaaaaaaaaaaaaa!`)
             .setImage("https://media1.tenor.com/images/e84edb279472c7ab49e97ec276d4ffda/tenor.gif?itemid=19354838")
-
-        return message.channel.send(goodNightEmbed);
+        );
     }
 }
