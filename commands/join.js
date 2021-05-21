b40ea534b16f01c23c6070ec97de4d2491cb78c3
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-21 02:12:56 -0300

    Add server verification in join.js

--- commands/join.js
+++ commands/join.js
@@ -9,6 +9,8 @@ module.exports = {
             return message.reply("you need to be on a voice channel to execute this command!");
         } else if (message.guild.me.voice.channel && message.guild.me.voice.channel.name == userVoiceChannel.name) {
             return message.reply("I'm already on the same channel as you!");
+        } else if (message.guild.me.voice.channel && !(message.guild.me.voice.channel.name == message.member.voice.channel.name)) {
+            return message.reply("I'm already on another voice channel on this server!");
         }
 
         userVoiceChannel.join();
