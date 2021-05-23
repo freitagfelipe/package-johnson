b93de888a06a65a62de5c3c8015028e14147d7b3
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-23 05:51:51 -0300

    Update sendQueueEmbed to accept more arguments

--- utils/sendQueueEmbed.js
+++ utils/sendQueueEmbed.js
@@ -1,12 +1,11 @@
 const { MessageEmbed } = require("discord.js");
-const ytdl = require("ytdl-core");
 const pagination = require("discord.js-pagination");
 
 module.exports = {
-    async sendQueueEmbed(message, queue) {
+    async sendQueueEmbed(message, musics, loopingMusic, loopingQueue) {
         const pages = [];
 
-        for (let i = 0; i < Math.ceil(queue.musics.length / 10); i++) {
+        for (let i = 0; i < Math.ceil(musics.length / 10); i++) {
             const page = new MessageEmbed()
                     .setAuthor(
                         `${message.client.user.username}`,
@@ -15,19 +14,16 @@ module.exports = {
                     .setColor("#FFFF00")
                     .setTitle("Music queue:")
                     .setThumbnail(`${message.client.user.avatarURL()}`);
-                    
 
             for (let j = 10 * i, number = 1; j < 10 * (i + 1); j++, number++) {
-                if (queue.musics[j] == undefined) {
+                if (musics[j] == undefined) {
                     break;
                 }
 
-                const musicInfo = await ytdl.getInfo(queue.musics[j].songURL);
-
-                page.addField("\u200B",`${number}) [${musicInfo.videoDetails.title}](${musicInfo.videoDetails.video_url})`);
+                page.addField("\u200B",`${number}) [${musics[j].songInfo.videoDetails.title}](${musics[j].songInfo.videoDetails.video_url})`);
             }
             
-            page.addField("\u200B", `Queue loop: ${queue.loopingQueue ? "✔️" : "❌"} || Music loop: ${queue.loopingMusic ? "✔️" : "❌"}`);
+            page.addField("\u200B", `Queue loop: ${loopingQueue ? "✔️" : "❌"} || Music loop: ${loopingMusic ? "✔️" : "❌"}`);
 
             pages.push(page);
         }
