24a3454159a8a4bc827b6a2561dba89aff355a23
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-23 03:43:04 -0300

    Add prefix in messages

--- interactions/greetingMessage.js
+++ interactions/greetingMessage.js
@@ -1,7 +1,9 @@
+const { prefix } = require("../config.json");
+
 module.exports = {
     name: "greeting message",
     
     execute(message) {
-        message.reply('hiii!!!! If you wanna see my command list just type: "!pj commands"!');
+        message.reply(`hiii!!!! If you wanna see my command list just type: "${prefix}commands"!`);
     }
 }
