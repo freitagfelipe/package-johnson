60f009e78ba4d4b75590bcbd309cb3d6cd2f0f54
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-27 00:15:03 -0300

    Add one aliase in join.js and update conditions

--- commands/join.js
+++ commands/join.js
@@ -1,20 +1,18 @@
 module.exports = {
     name: "join",
     description: "Package Johnson will join in your voice channel.",
-    aliases: ["connect"],
+    aliases: ["connect", "j"],
 
     execute(message) {
-        const userVoiceChannel = message.member.voice.channel;
-
-        if (!userVoiceChannel) {
+        if (!message.member.voice.channel) {
             return message.reply("you need to be on a voice channel to execute this command!");
-        } else if (message.guild.me.voice.channel && message.guild.me.voice.channel.name == userVoiceChannel.name) {
+        } else if (message.guild.me.voice.channel && message.guild.me.voice.channel.name == message.member.voice.channel.name) {
             return message.reply("I'm already on the same channel as you!");
         } else if (message.guild.me.voice.channel && !(message.guild.me.voice.channel.name == message.member.voice.channel.name)) {
             return message.reply("I'm already on another voice channel on this server!");
         }
 
-        userVoiceChannel.join();
+        message.member.voice.channel.join();
 
         return message.reply("successfully connected");
     }
