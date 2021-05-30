da824216de61937ea5cf6dd2afc94adc9908104f
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-05-30 07:14:54 -0300

    Add search.js command

---
+++ commands/search.js
@@ -0,0 +1,97 @@
+const client = require("../package-johnson");
+const ytsr = require("ytsr");
+const { MessageEmbed, Collector } = require("discord.js");
+const { embedColor } = require("../config.json");
+
+module.exports = {
+    name: "search",
+    description: "Search for a music name and send a list to your choose wich one will play.",
+
+    async execute(message, args) {
+        if (!message.member.voice.channel) {
+            return message.reply("you need to be on a voice channel to execute this command!");
+        } else if (!(args.length > 0)) {
+            return message.reply("you need to inser a music name!");
+        }
+
+        let searchingMessage;
+
+        message.channel.send("**Searching!**").then(msg => {
+            searchingMessage = msg;
+        });
+
+        const result = await ytsr(args.join(" "), {pages: 1});
+
+        searchingMessage.delete();
+
+        if(!result.items.length) {
+            return message.reply("no song ware found! Please try again.");
+        }
+
+        const musics = result.items.filter(item => item.type == "video");
+
+        const page = new MessageEmbed()
+            .setAuthor(
+                `${message.client.user.username}`,
+                `${message.client.user.displayAvatarURL()}`
+            )
+            .setColor(embedColor)
+            .setThumbnail(`${message.client.user.avatarURL()}`)
+            .setTitle("React with the emoji of the music you want to listen to!")
+
+        let musicText = "";
+
+        for (let i = 0; i < 5; i++) {
+            musicText += `${i + 1}) [${musics[i].title}](${musics[i].url}) | [${musics[i].author.name}](${musics[i].author.url})\n`;
+        }
+
+        page.addField("These are the results of your search: ", musicText);
+
+        message.channel.send(page).then(msg => {
+            const filter = (reaction, user) => {
+                return ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"].includes(reaction.emoji.name) && user.id === message.author.id;
+            }
+
+            const collector = msg.createReactionCollector(filter, { time: 60000});
+
+            setTimeout(() => {
+                if (!msg.deleted) {
+                    message.channel.send("**Timeout!**");
+                    message.delete();
+                    msg.delete();
+                }
+            }, 60000);
+
+            collector.on("collect", collected => {
+                let music;
+    
+                switch (collected.emoji.name) {
+                    case "1️⃣":
+                        music = musics[0].url;
+                        break;
+                    case "2️⃣":
+                        music = musics[1].url;
+                        break;
+                    case "3️⃣":
+                        music = musics[2].url;
+                        break;
+                    case "4️⃣":
+                        music = musics[3].url;
+                        break;
+                    case "5️⃣":
+                        music = musics[4].url;
+                        break;
+                }
+    
+                if (music) {
+                    msg.delete();
+                    message.delete();
+
+                    client.commands.get("play").execute(message, [music]);
+                } else {
+                    message.reply("there is no song with the numer you reacted, please react with another number!");
+                }
+            });
+        });
+    }
+}
