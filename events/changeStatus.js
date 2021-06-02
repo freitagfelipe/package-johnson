83338033d3f9428e3b13ddc021142c9a239b7416
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-06-02 09:59:16 -0300

    Update changeStatus to avoid bugs and add activities

--- events/changeStatus.js
+++ events/changeStatus.js
@@ -1,25 +1,24 @@
 const client = require("../package-johnson");
+const { prefix } = require("../config.json");
+const { getCurrentInteraction, getAwakeTime } = require("../utils/timeUtils");
 
 module.exports = {
     changeStatus() {
-        let hour = "";
-
-        setInterval(() => {
-            hour = new Date().getHours();
-        }, 80000);
-
-        const activities = [
-            "Say hi to @Package Johnson!",
-            `I'm in ${client.guilds.cache.size} servers!`,
-            `${hour >= 6 && hour <= 12 ? "Say good morning to @Package Johnson!" : hour > 12 && hour <= 18 ? "Say good afternoon to @Package Johnson!" : "Say good night to @Package Johnson!"}`,
-            ".pj commands to see my command list!",
-            ".pj playlist to see my favorites lofi!",
-            "Do you know that I am open source?https://github.com/freitagfelipe/package-johnson-discord",
-            ".pj help if you have any questions about a command!"
-        ]
-
         let i = 0;
 
-        setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`), 30000);
+        setInterval(() => {
+            const activities = [
+                "Say hi to @Package Johnson!",
+                `I'm in ${client.guilds.cache.size} servers!`,
+                `${getCurrentInteraction()}`,
+                `${prefix}commands to see my command list!`,
+                `${prefix}playlist to see my favorites lofi!`,
+                "Do you know that I'm open source?https://github.com/freitagfelipe/package-johnson-discord",
+                `${prefix}help if you have any questions about a command!`,
+                `I've been awake for ${getAwakeTime()}!`,
+            ]
+
+            client.user.setActivity(`${activities[i++ % activities.length]}`);
+        }, 10000);
     }
 }
