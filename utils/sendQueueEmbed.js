31f9b97f030f8e2ff882f863050223db38fc7860
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-06-03 09:09:00 -0300

    Add description in sendQueueEmbed.js

--- utils/sendQueueEmbed.js
+++ utils/sendQueueEmbed.js
@@ -5,12 +5,8 @@ const { embedColor } = require("../config.json");
 module.exports = {
     async sendQueueEmbed(message, musics, loopingMusic, loopingQueue) {
         const pages = [];
-        const nowPlaying = musics[0]
-
-        for (let i = 0; i < Math.ceil(musics.length / 10); i++) {
-            let musicText = "";
-
-            const page = new MessageEmbed()
+        let descriptionText = "";
+        let currentPage = new MessageEmbed()
                     .setAuthor(
                         `${message.client.user.username}`,
                         `${message.client.user.displayAvatarURL()}`
@@ -18,27 +14,37 @@ module.exports = {
                     .setColor(`${embedColor}`)
                     .setTitle(`Queue for ${message.guild.name}`)
                     .setThumbnail(`${message.client.user.avatarURL()}`)
-                    .addField("**Now Playing:**", `[${nowPlaying.songInfo.videoDetails.title}](${nowPlaying.songInfo.videoDetails.video_url}) | \`${nowPlaying.user.username}#${nowPlaying.user.discriminator}\``)
 
-            for (let j = 10 * i; j < 10 * (i + 1); j++) {
-                if (musics[j] == undefined) {
-                    break;
-                } else if (j != 0) {
-                    musicText += `${j}) [${musics[j].songInfo.videoDetails.title}](${musics[j].songInfo.videoDetails.video_url}) | \`${musics[j].user.username}#${musics[j].user.discriminator}\`\n`;
-                }
+        for (let i = 0; i < musics.length; i++) {
+            if (i == 0) {
+                descriptionText += `**Now Playing:**\n- [${musics[i].songInfo.videoDetails.title}](${musics[i].songInfo.videoDetails.video_url}) | \`${musics[i].user.username}#${musics[i].user.discriminator}\`\n`;
+            } else if(i == 1) {
+                descriptionText += `**Up Next:**\n${i}) [${musics[i].songInfo.videoDetails.title}](${musics[i].songInfo.videoDetails.video_url}) | \`${musics[i].user.username}#${musics[i].user.discriminator}\`\n`;
+            } else {
+                descriptionText += `${i}) [${musics[i].songInfo.videoDetails.title}](${musics[i].songInfo.videoDetails.video_url}) | \`${musics[i].user.username}#${musics[i].user.discriminator}\`\n`;
             }
 
-            if (musicText) {
-                page.addField("**Up Next:**", `${musicText}`);
-            }
-            
-            page.addField("\u200B", `Queue loop: ${loopingQueue ? "✅" : "❎"} || Music loop: ${loopingMusic ? "✅" : "❎"}`);
+            if ((i % 10 == 0 && i != 0) || i == musics.length - 1) {
+                currentPage.setDescription(descriptionText);
+                currentPage.addField("\u200B", `Queue loop: ${loopingQueue ? "✅" : "❎"} || Music loop: ${loopingMusic ? "✅" : "❎"}`);
 
-            pages.push(page);
+                pages.push(currentPage);
+
+                descriptionText = "";
+                currentPage = new MessageEmbed()
+                    .setAuthor(
+                        `${message.client.user.username}`,
+                        `${message.client.user.displayAvatarURL()}`
+                    )
+                    .setColor(`${embedColor}`)
+                    .setTitle(`Queue for ${message.guild.name}`)
+                    .setThumbnail(`${message.client.user.avatarURL()}`)
+            }
         }
 
         if (pages.length == 1) {
             pages[0].setFooter("Page 1/1");
+            
             return message.channel.send(pages[0]);
         } else {
             return pagination(message, pages, ['⏪', '⏩'], "60000");
