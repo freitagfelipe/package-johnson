43e7f4c18ec99de8cd28bf0193ba4aa03e5af57e
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-06-06 09:38:34 -0300

    Add help.js in commands

---
+++ commands/help.js
@@ -0,0 +1,39 @@
+const client = require("../package-johnson");
+
+const { MessageEmbed } = require("discord.js");
+const { embedColor } = require("../config.json");
+
+module.exports = {
+    name: "help",
+    description: "Teaches you how to use a command.",
+    aliases: ["h"],
+    usage: ".pj help <command name>",
+
+    execute(message, args) {
+        if (args.length == 0) {
+            return message.reply("you need to insert the command name to get help!");
+        }
+
+        const command = client.commands.get(args[0]) || client.commands.find(command => command.aliases && command.aliases.includes(args[0]));
+
+        if (!command) {
+            return message.reply(`I couldn't find the command ${args[0]}! If you want to see a complete list of my commands type \`.pj commands\`!`);
+        }
+
+        return message.channel.send(new MessageEmbed()
+            .setAuthor(
+                `${client.user.username}`,
+                `${client.user.displayAvatarURL()}`
+            )
+            .setColor(embedColor)
+            .setTitle(`I'll help you with \`${command.name}\`!`)
+            .addFields(
+                { name: "Aliases:", value: `\`${command.aliases}\``},
+                { name: "Description:", value: `\`${command.description}\``},
+                { name: "Usage:", value: `\`${command.usage}\``}
+            )
+            .setThumbnail(`${client.user.displayAvatarURL()}`)
+            .setTimestamp()
+        )
+    }
+}
