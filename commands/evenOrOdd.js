129e52606fa60b07207513773c734d3b667304f5
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-06-18 17:12:51 -0300

    Add evenOrOdd.js in commands

---
+++ commands/evenOrOdd.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+const { embedColor } = require("../config.json");
+
+module.exports = {
+    name: "even-or-odd",
+    description: "Package Johnson will play odd or even with you.",
+    aliases: ["eo", "evenorodd"],
+    usage: ".pj even-or-odd <even> <number> or .pj even-or-odd <odd> <number>",
+
+    execute(message, args) {
+        if (args.length < 2 || (args[0] != "even" && args[0] != "odd") || isNaN(args[1])) {
+            return message.reply("the correct usage of this command is .pj even-or-odd <even> <number> or .pj even-or-odd <odd> <number>!");
+        }
+
+        const packageNumber = Math.floor(Math.random() * 11);
+        let winnerName, winnerAvatar;
+
+        if (args[0] == "even") {
+            winnerName = (packageNumber + parseInt(args[1])) % 2 == 0 ? `${message.author.username}` : `${message.client.user.username}`;
+            winnerAvatar = (packageNumber + parseInt(args[1])) % 2 == 0 ? `${message.author.displayAvatarURL()}` : `${message.client.user.displayAvatarURL()}`;
+        } else {
+            winnerName = (packageNumber + parseInt(args[1])) % 2 != 0 ? `${message.author.username}` : `${message.client.user.username}`;
+            winnerAvatar = (packageNumber + parseInt(args[1])) % 2 != 0 ? `${message.author.displayAvatarURL()}` : `${message.client.user.displayAvatarURL()}`;
+        }
+
+        return message.channel.send(new MessageEmbed()
+            .setAuthor(
+                `${message.client.user.username}`,
+                `${message.client.user.displayAvatarURL()}`
+            )
+            .setColor(embedColor)
+            .setTitle(`${winnerName} won this match!`)
+            .addFields(
+                { name: `${message.author.username} played:`, value: `${args[0]} and choose the number ${args[1]}!`},
+                { name: "Package Johnson played:", value: `${args[0] == "even" ? "odd" : "even"} and choose the number ${packageNumber}!`}
+            )
+            .setThumbnail(`${winnerAvatar}`)
+            .setTimestamp()
+        )
+    }
+}
