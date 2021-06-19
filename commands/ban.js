38651d38701384356eba8915512700d045cf373e
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-06-18 23:31:29 -0300

    Update ban.js and kick.js to avoid permissions errors

--- commands/ban.js
+++ commands/ban.js
@@ -19,26 +19,28 @@ module.exports = {
             return message.reply("the correct usage of this command is .pj ban <user mention> or .pj ban <user mention> <reason>!");
         } else if (member.id == message.author.id) {
             return message.reply("you can't ban yourself!");
+        } else if (member.id == message.client.user.id) {
+            return message.reply("I can't ban myself!");
         }
 
         const reason = args.slice(1).join(" ") || "No reason";
 
-        try {
-            member.ban({ reason: reason }).then(() => {
-                message.channel.send(new MessageEmbed()
-                    .setAuthor(
-                        `${message.client.user.username}`,
-                        `${message.client.user.displayAvatarURL()}`
-                    )
-                    .setColor(embedColor)
-                    .setTitle(`${member.user.username} was banned from the server by ${message.author.username}!🔨`)
-                    .setDescription(`Reason: ${reason}`)
-                    .setThumbnail(member.user.displayAvatarURL())
-                    .setTimestamp()
+        member.ban({ reason: reason }).then(() => {
+            message.channel.send(new MessageEmbed()
+                .setAuthor(
+                    `${message.client.user.username}`,
+                    `${message.client.user.displayAvatarURL()}`
                 )
-            });
-        } catch(error) {
-            return message.reply(`I don't have permission to kick this user because his role is higher than mine!`);
-        }
+                .setColor(embedColor)
+                .setTitle(`${member.user.username} was banned from the server by ${message.author.username}!🔨`)
+                .setDescription(`Reason: ${reason}`)
+                .setThumbnail(member.user.displayAvatarURL())
+                .setTimestamp()
+            )
+        }).catch(error => {
+            console.log(error);
+
+            return message.reply("I don't have permission to ban this user because his role is higher than mine!");
+        });
     }
 }
