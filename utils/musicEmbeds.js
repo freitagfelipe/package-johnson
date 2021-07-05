fd6e61748a4cab26e69c3af02c5a9824c1582692
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-07-05 09:15:58 -0300

    Merge music embeds utils in a single file

--- utils/musicEmbeds.js
+++ utils/musicEmbeds.js
@@ -1,9 +1,42 @@
 const pagination = require("discord.js-pagination");
-const { formatMusicTime } = require("../utils/timeUtils");
 const { MessageEmbed } = require("discord.js");
+const { formatMusicTime } = require("./timeUtils");
 const { embedColor } = require("../config.json");
 
 module.exports = {
+    sendNowPlayingMusicEmbed(message, music, dispatcher) {
+        const musicTime = formatMusicTime(Number(music.songInfo.videoDetails.lengthSeconds));
+        const currentTime = formatMusicTime(dispatcher.streamTime / 1000);
+        const percentTime = Math.floor(30 * ((dispatcher.streamTime / 1000) / Number(music.songInfo.videoDetails.lengthSeconds)));
+        let timeBar = "";
+
+        for (let i = 0; i < 30; i++) {
+            if (i == percentTime) {
+                timeBar += "🔘";
+            } else {
+                timeBar += "▬";
+            }
+        }
+
+        return message.channel.send(new MessageEmbed()
+            .setAuthor(
+                `${message.client.user.username}`,
+                `${message.client.user.displayAvatarURL()}`
+            )
+            .setColor(`${embedColor}`)
+            .setTitle(`Music name: ${music.songInfo.videoDetails.title}`)
+            .setURL(`${music.songInfo.videoDetails.video_url}`)
+            .addFields(
+                { name: "Channel Name:", value: `[${music.songInfo.videoDetails.author.name}](${music.songInfo.videoDetails.author.user_url})`, inline: true},
+                { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true},
+                { name: "\u200B", value: `\`${timeBar}\``},
+                { name: "Current time:", value: `\`${currentTime} / ${musicTime}\``},
+            )
+            .setThumbnail(music.songInfo.videoDetails.thumbnails[0].url)
+            .setTimestamp()
+        );
+    },
+
     async sendQueueEmbed(message, musics, loopingMusic, loopingQueue) {
         const pages = [];
         let currentPage = new MessageEmbed()
@@ -62,5 +95,27 @@ module.exports = {
                 }, 60000);
             });
         }
+    },
+
+    sendMusicEmbed(message, musics, songInfo, wichPlay) {
+        const musicTime = formatMusicTime(Number(songInfo.videoDetails.lengthSeconds));
+
+        message.channel.send(new MessageEmbed()
+            .setAuthor(
+                `${message.client.user.username}`,
+                `${message.client.user.displayAvatarURL()}`
+            )
+            .setColor(`${embedColor}`)
+            .setTitle(`Music name: ${songInfo.videoDetails.title}`)
+            .setURL(`${songInfo.videoDetails.video_url}`)
+            .addFields(
+                { name: "Channel Name:", value: `[${songInfo.videoDetails.author.name}](${songInfo.videoDetails.author.user_url})`, inline: true },
+                { name: "Duration time:", value: `\`${musicTime}\``, inline: true },
+                { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true}
+            )
+            .setThumbnail(songInfo.videoDetails.thumbnails[0].url)
+            .setFooter(`• Queue position: ${musics.length - 1 == 0 || wichPlay == 2 ? "now playing" : wichPlay == 1 ? "1" : musics.length - 1} || `)
+            .setTimestamp()
+        );
     }
 }
