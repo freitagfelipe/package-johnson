142921f3b2486505e15216826afaeff87769f89d
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-04 15:23:21 -0300

    Update musicEmbeds and queue utils to discord.js v13

--- utils/musicEmbeds.js
+++ utils/musicEmbeds.js
@@ -1,13 +1,13 @@
-const pagination = require("discord.js-pagination");
+const paginationEmbed = require("@freitagfelipe/discord.js-pagination");
 const { MessageEmbed } = require("discord.js");
 const { formatMusicTime } = require("./timeUtils");
 const { embedColor } = require("../config.json");
 
 module.exports = {
-    sendNowPlayingMusicEmbed(message, music, dispatcher) {
+    sendNowPlayingMusicEmbed(message, music, player) {
         const musicTime = formatMusicTime(Number(music.songInfo.videoDetails.lengthSeconds));
-        const currentTime = formatMusicTime(dispatcher.streamTime / 1000);
-        const percentTime = Math.floor(30 * ((dispatcher.streamTime / 1000) / Number(music.songInfo.videoDetails.lengthSeconds)));
+        const currentTime = formatMusicTime(player.state.playbackDuration / 1000);
+        const percentTime = Math.floor(30 * ((player.state.playbackDuration / 1000) / Number(music.songInfo.videoDetails.lengthSeconds)));
         let timeBar = "";
 
         for (let i = 0; i < 30; i++) {
@@ -18,35 +18,43 @@ module.exports = {
             }
         }
 
-        return message.channel.send(new MessageEmbed()
+        return message.channel.send({ embeds: [
+            new MessageEmbed()
+                .setAuthor(
+                    `${message.client.user.username}`,
+                    `${message.client.user.displayAvatarURL()}`
+                )
+                .setColor(`${embedColor}`)
+                .setTitle(`Music name: ${music.songInfo.videoDetails.title}`)
+                .setURL(`${music.songInfo.videoDetails.video_url}`)
+                .addFields(
+                    { name: "Channel Name:", value: `[${music.songInfo.videoDetails.author.name}](${music.songInfo.videoDetails.author.user_url})`, inline: true},
+                    { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true},
+                    { name: "\u200B", value: `\`${timeBar}\``},
+                    { name: "Current time:", value: `\`${currentTime} / ${musicTime}\``},
+                )
+                .setThumbnail(music.songInfo.videoDetails.thumbnails[0].url)
+                .setTimestamp()
+        ] });
+    },
+
+    async sendQueueEmbed(message, musics, loopingMusic, loopingQueue) {
+        let currentPage = new MessageEmbed()
             .setAuthor(
                 `${message.client.user.username}`,
                 `${message.client.user.displayAvatarURL()}`
             )
             .setColor(`${embedColor}`)
-            .setTitle(`Music name: ${music.songInfo.videoDetails.title}`)
-            .setURL(`${music.songInfo.videoDetails.video_url}`)
-            .addFields(
-                { name: "Channel Name:", value: `[${music.songInfo.videoDetails.author.name}](${music.songInfo.videoDetails.author.user_url})`, inline: true},
-                { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true},
-                { name: "\u200B", value: `\`${timeBar}\``},
-                { name: "Current time:", value: `\`${currentTime} / ${musicTime}\``},
-            )
-            .setThumbnail(music.songInfo.videoDetails.thumbnails[0].url)
-            .setTimestamp()
-        );
-    },
-
-    async sendQueueEmbed(message, musics, loopingMusic, loopingQueue) {
+            .setTitle(`Queue for ${message.guild.name}`)
+            .setThumbnail(`${message.client.user.avatarURL()}`);
         const pages = [];
-        let currentPage = new MessageEmbed()
-                    .setAuthor(
-                        `${message.client.user.username}`,
-                        `${message.client.user.displayAvatarURL()}`
-                    )
-                    .setColor(`${embedColor}`)
-                    .setTitle(`Queue for ${message.guild.name}`)
-                    .setThumbnail(`${message.client.user.avatarURL()}`)
+        const endPage = new MessageEmbed()
+            .setAuthor(
+                `${message.client.user.username}`,
+                `${message.client.user.displayAvatarURL()}`
+            )
+            .setColor(`${embedColor}`)
+            .setDescription("**Timeout!⌛**");
 
         for (let i = 0; i < musics.length; i++) {
             const durationTime = formatMusicTime(musics[i].songInfo.videoDetails.lengthSeconds);
@@ -74,48 +82,29 @@ module.exports = {
             }
         }
 
-        if (pages.length == 1) {
-            pages[0].setFooter("Page 1/1");
-            
-            return message.channel.send(pages[0]).then(msg => {
-                setTimeout(() => {
-                    if (!msg.deleted) {
-                        msg.delete();
-                        message.delete();
-                    }
-                }, 60000);
-            });
-        } else {
-            return pagination(message, pages, ['⏪', '⏩'], 60000).then(msg => {
-                setTimeout(() => {
-                    if (!msg.deleted) {
-                        msg.delete();
-                        message.delete();
-                    }
-                }, 60000);
-            });
-        }
+        return paginationEmbed(message, pages, 60000, ['⏪', '⏩'], false, endPage);
     },
 
     sendMusicEmbed(message, musics, songInfo, wichPlay) {
         const musicTime = formatMusicTime(Number(songInfo.videoDetails.lengthSeconds));
 
-        message.channel.send(new MessageEmbed()
-            .setAuthor(
-                `${message.client.user.username}`,
-                `${message.client.user.displayAvatarURL()}`
-            )
-            .setColor(`${embedColor}`)
-            .setTitle(`Music name: ${songInfo.videoDetails.title}`)
-            .setURL(`${songInfo.videoDetails.video_url}`)
-            .addFields(
-                { name: "Channel Name:", value: `[${songInfo.videoDetails.author.name}](${songInfo.videoDetails.author.user_url})`, inline: true },
-                { name: "Duration time:", value: `\`${musicTime}\``, inline: true },
-                { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true}
-            )
-            .setThumbnail(songInfo.videoDetails.thumbnails[0].url)
-            .setFooter(`• Queue position: ${musics.length - 1 == 0 || wichPlay == 2 ? "now playing" : wichPlay == 1 ? "1" : musics.length - 1} || `)
-            .setTimestamp()
-        );
+        message.channel.send({ embeds: [
+            new MessageEmbed()
+                .setAuthor(
+                    `${message.client.user.username}`,
+                    `${message.client.user.displayAvatarURL()}`
+                )
+                .setColor(`${embedColor}`)
+                .setTitle(`Music name: ${songInfo.videoDetails.title}`)
+                .setURL(`${songInfo.videoDetails.video_url}`)
+                .addFields(
+                    { name: "Channel Name:", value: `[${songInfo.videoDetails.author.name}](${songInfo.videoDetails.author.user_url})`, inline: true },
+                    { name: "Duration time:", value: `\`${musicTime}\``, inline: true },
+                    { name: "Requested by:", value: `\`${message.author.username}#${message.author.discriminator}\``, inline: true}
+                )
+                .setThumbnail(songInfo.videoDetails.thumbnails[0].url)
+                .setFooter(`• Queue position: ${musics.length - 1 == 0 || wichPlay == 2 ? "now playing" : wichPlay == 1 ? "1" : musics.length - 1} || `)
+                .setTimestamp()
+        ] });
     }
 }
