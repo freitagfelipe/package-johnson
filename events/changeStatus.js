a2bbe433740fc4b6eb4e104c31b38b26923af8c5
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-04 17:14:17 -0300

    Add a condition to reset i in changeStatus.js

--- events/changeStatus.js
+++ events/changeStatus.js
@@ -7,18 +7,22 @@ module.exports = {
         let i = 0;
 
         setInterval(() => {
+            if (i === 8) {
+                i = 0;
+            }
+
             const activities = [
                 "Say hi to @Package Johnson!",
                 `I'm in ${client.guilds.cache.size} servers!`,
                 `${getCurrentInteraction()}`,
-                `${prefix}commands to see my command list!`,
-                `${prefix}playlist to see my favorites lofi!`,
+                `${prefix} commands to see my command list!`,
+                `${prefix} playlist to see my favorites lofi!`,
                 "Do you know that I'm open source? https://github.com/freitagfelipe/package-johnson-discord",
-                `${prefix}help if you have any questions about a command!`,
+                `${prefix} help <command name> if you have any questions about a command!`,
                 `I've been awake for ${getAwakeTime()}!`,
             ]
 
-            return client.user.setActivity(`${activities[i++ % activities.length]}`);
+            return client.user.setActivity(`${activities[i++]}`);
         }, 10000);
     }
 }
