d55a1575e8ca1f1ed2e21530a03a0056012fb5a8
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-04 04:06:13 -0300

    Update interactions to discord.js v13

--- interactions/goodMorning.js
+++ interactions/goodMorning.js
@@ -5,14 +5,15 @@ module.exports = {
     name: "good morning",
 
     execute(message) {
-        return message.channel.send(new MessageEmbed()
-            .setAuthor(
-                "Package Johnson",
-                "https://i.imgur.com/vDVSu00.png"
-            )
-            .setColor(`${embedColor}`)
-            .setTitle(`Ohayoooooooooo!`)
-            .setImage("https://i.pinimg.com/originals/1d/9b/5e/1d9b5e8047d1ec289bfc698ebb4c64cc.gif")
-        );
+        return message.channel.send({ embeds: [
+            new MessageEmbed()
+                .setAuthor(
+                    "Package Johnson",
+                    "https://i.imgur.com/vDVSu00.png"
+                )
+                .setColor(`${embedColor}`)
+                .setTitle(`Ohayoooooooooo!`)
+                .setImage("https://i.pinimg.com/originals/1d/9b/5e/1d9b5e8047d1ec289bfc698ebb4c64cc.gif")
+        ] });
     }
 }
--- interactions/goodNight.js
+++ interactions/goodNight.js
@@ -5,14 +5,15 @@ module.exports = {
     name: "good night",
 
     execute(message) {
-        return message.channel.send(new MessageEmbed()
-            .setAuthor(
-                "Package Johnson",
-                "https://i.imgur.com/vDVSu00.png"
-            )
-            .setColor(`${embedColor}`)
-            .setTitle(`Konbanwaaaaaaaaaaaa!`)
-            .setImage("https://data.whicdn.com/images/335787701/original.gif")
-        );
+        return message.channel.send({ embeds: [
+            new MessageEmbed()
+                .setAuthor(
+                    "Package Johnson",
+                    "https://i.imgur.com/vDVSu00.png"
+                )
+                .setColor(`${embedColor}`)
+                .setTitle(`Konbanwaaaaaaaaaaaa!`)
+                .setImage("https://data.whicdn.com/images/335787701/original.gif")
+        ] });
     }
 }
