0610a6a3c8a53816d27b0a0e18dbbf386c2d4adf
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-04 04:43:13 -0300

    Add about.js in commands

---
+++ commands/about.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js");
+const { embedColor } = require("../config.json");
+
+module.exports = {
+    name: "about",
+    description: "Send a message with informations about Package Johnson creation.",
+    usage: ".pj about",
+
+    execute(message) {
+        return message.channel.send({ embeds: [
+            new MessageEmbed()
+                .setAuthor(
+                    `${message.client.user.username}`,
+                    `${userAvatar}`
+                )
+                .setColor(`${embedColor}`)
+                .setDescription('Initially, Package Johnson was an inside joke, caused by my confusion around the name package.json, which I misread as "package johnson". But then I saw an opportunity: what if I could make a Bot that would help me learn more about programming and that could honor that joke? With all that in mind, I decided to transform Package Johnson, that was just an inside joke, into a real thing!')
+        ] });
+    }
+}
