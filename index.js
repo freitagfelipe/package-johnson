c6dc023ef605058ad2e3f20801e756e0256b3fb1
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-10 03:53:05 -0300

    Add client in global, update changeStatus path in index.js and move guildCreate to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Client, Collection } = require("discord.js");
-const { prefix } = require("./config.json");
+const { Client, Collection, MessageEmbed } = require("discord.js");
+const { prefix, embedColor } = require("./config.json");
 const { connect } = require("./database/connect");
+const { changeStatus } = require("./utils/changeStatus");
 const dotenv = require("dotenv");
 const fileSystem = require("fs");
 
@@ -8,12 +9,12 @@ const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGE_REACTIONS", "GUIL
 
 dotenv.config();
 
-global.queues = [];
-
 client.on("ready", async () => {
     console.log("I'm Package Johnson and I'm ready!");
 
     global.pgClient = await connect();
+    global.client = client;
+    global.queues = [];
 
     changeStatus();
 
@@ -65,8 +66,49 @@ client.on("messageCreate", message => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.on('guildCreate', async guild => {
+    let channelMessageVerification = true;
 
-module.exports = client;
-require("./events/guildCreate");
-const { changeStatus } = require("./events/changeStatus");
+    guild.channels.cache.forEach(channel => {
+        if (channelMessageVerification && channel.type === "GUILD_TEXT" && channel.name === "general") {
+            channel.send({ embeds: [
+                new MessageEmbed()
+                    .setAuthor(
+                        `${client.user.username}`,
+                    )
+                    .setColor(`${embedColor}`)
+                    .setDescription(`Thanks for adding me to your server, my prefix is: \`${prefix}\` !`)
+                    .setThumbnail(`${client.user.avatarURL()}`)
+            ] });
+            
+            channelMessageVerification = false;
+        }
+    });
+
+    guild.roles.create({
+        name: "Package Johnson",
+        color: `${embedColor}`
+    }).then(role => {
+        guild.me.roles.add(role);
+        role.setHoist(true);
+    });
+
+    client
+
+    const owner = await guild.members.fetch(guild.ownerId);
+
+    return owner.send({ embeds: [
+        new MessageEmbed()
+            .setAuthor(
+                `${client.user.username}`,
+                `${client.user.displayAvatarURL()}`
+            )
+            .setColor(`${embedColor}`)
+            .setTitle(`Acknowledgments and informations.`)
+            .setDescription(`Hiiii, ${owner.user.username}! Thank you for add me in your discord server and if you want to see my command list you just need to click on this link: [Package Johnson repository](https://github.com/freitagfelipe/package-johnson-discord)!`)
+            .setImage(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
+            .setTimestamp()
+    ] });
+});
+
+client.login(process.env.TOKEN);
