e8315803f6d40019e3b8a2ec75d7c7d13dcafdfe
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-11 04:09:07 -0300

    Update setInterval in changeStatus and add variables to recieve functions return

--- utils/changeStatus.js
+++ utils/changeStatus.js
@@ -6,20 +6,23 @@ module.exports = {
         const client = global.client;
         let i = 0;
 
-        setInterval(() => {
+        setInterval(async () => {
             if (i === 8) {
                 i = 0;
             }
 
+            const currentInteraction = getCurrentInteraction();
+            const awakeTime = await getAwakeTime();
+
             const activities = [
                 "Say hi to @Package Johnson!",
                 `I'm in ${client.guilds.cache.size} servers!`,
-                `${getCurrentInteraction()}`,
+                `${currentInteraction}`,
                 `${prefix} commands to see my command list!`,
                 `${prefix} playlist to see my favorites lofi!`,
                 "Do you know that I'm open source? https://github.com/freitagfelipe/package-johnson-discord",
                 `${prefix} help <command name> if you have any questions about a command!`,
-                `I've been awake for ${getAwakeTime()}!`,
+                `I've been awake for ${awakeTime}!`,
             ]
 
             return client.user.setActivity(`${activities[i++]}`);
